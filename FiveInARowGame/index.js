93ed45fee986cec48fba2cdfb8aa59a45c003401
HappyHooman <tornea.alexandru@gmail.com>
2016-07-14 15:37:21 +0300

    added adjustable size of the grid the 5 winning marks are marked with red bold color added a bug (when last mark of the 5 has a neighbour on a different line, it's marked too)

--- FiveInARowGame/index.js
+++ FiveInARowGame/index.js
@@ -1,4 +1,4 @@
-var turn = "x";
+var turn = "x";  //specifies who's turn is now
 var table = [
     ["", "", "", "", "", "", "", "",],
     ["", "", "", "", "", "", "", "",],
@@ -8,7 +8,13 @@ var table = [
     ["", "", "", "", "", "", "", "",],
     ["", "", "", "", "", "", "", "",],
     ["", "", "", "", "", "", "", "",],
-];
+]; //initialize the grid table
+
+var marked = [];
+var props = {
+    width: 600,
+    height: 600
+};
 
 
 function startGame() {
@@ -18,17 +24,16 @@ function startGame() {
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
-        this.canvas.width = 600;
-        this.canvas.height = 600;
+        this.canvas.width = props.width;
+        this.canvas.height = props.height;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        var me = this.canvas;
         console.log("Click any cell to begin playing the game. Five in a row wins!");
         this.canvas.addEventListener('mousedown', function (e) {
             var x = e.offsetX;
             var y = e.offsetY;
-            var width = me.width / 8;
-            var height = me.height / 8;
+            var width = props.width / 8;
+            var height = props.height / 8;
             x = Math.floor(x / width);
             y = Math.floor(y / height);
             if (e.button == 0 && (turn == "x" || turn == "o")) {
@@ -45,8 +50,8 @@ var myGameArea = {
 function drawGrid() {
     var ctx = myGameArea.context;
     ctx.lineWidth = 3;
-    var width = 600 / 8;
-    var height = 600 / 8;
+    var width = props.width / 8;
+    var height = props.height / 8;
 
     //horizontal lines
     for (var i = 0; i < 8; i++) {
@@ -65,9 +70,12 @@ function drawGrid() {
 
 
 function checkDirection(x, y, n1, n2) {
+    // x, y - the coordinates of the current cell
+    // n1, n2 - specify the direction (may only have the values "-1", "0" and "1")
     var r = 0;
     if (x + n1 >= 0 && x + n1 < 8 && y + n2 >= 0 && y + n2 < 8) {
         if (table[x + n1][y + n2] == table[x][y]) {
+            marked.push([x + n1, y + n2]);
             r++;
             if (r == 4) return r;
             r += checkDirection(x + n1, y + n2, n1, n2)
@@ -76,18 +84,38 @@ function checkDirection(x, y, n1, n2) {
     return r;
 }
 
-function wins(x, y) {
+function printMarked(winner) {
+    var l = marked.length;
+    if (winner == "x") {
+        for (var i = 0; i < 5; i++) {
+            drawX(marked[i][0], marked[i][1], "red");
+        }
+    }
+    if (winner == "o") {
+        for (var i = 0; i < 5; i++) {
+            drawO(marked[i][0], marked[i][1], "red");
+        }
+    }
+}
+function wins(winner) {
+    myGameArea.context.lineWidth = 10;
+    myGameArea.context.lineCap = "round";
+    printMarked(winner);
     turn = "0";
-    console.log("Player " + table[x][y].toUpperCase() + " wins!!!");
+    console.log("Player " + winner.toUpperCase() + " wins!!!");
 }
 function checkNeighbours(x, y) {
     var c = 1;
+
+    marked = [];
+    marked.push([x, y]);
 
     c += checkDirection(x, y, -1, -1);
     c += checkDirection(x, y, 1, 1);
     if (c >= 5) console.log(c + " - on left diagonal");
 
     if (c < 5) {
+
         c = 1;
         c += checkDirection(x, y, 0, -1);
         c += checkDirection(x, y, 0, 1);
@@ -106,7 +134,7 @@ function checkNeighbours(x, y) {
         if (c >= 5) console.log(c + " - horizontally");
     }
 
-    if (c == 5) wins(x, y);
+    if (c == 5) wins(table[x][y]);
 }
 
 function draw(x, y) {
@@ -121,15 +149,20 @@ function draw(x, y) {
     checkNeighbours(x, y);
 }
 
-function drawX(x, y) {
-    var width = 600 / 8;
-    var height = 600 / 8;
+function drawX(x, y, color) {
+    var width = props.width / 8;
+    var height = props.height / 8;
     var ctx = myGameArea.context;
     var posX, posY;
+
+    if (!color) color = "#000";
+    ctx.strokeStyle = color;
 
     posX = x * width + width / 2;
     posY = y * height + height / 2;
 
+
+    ctx.beginPath();
     ctx.translate(posX, posY);
     ctx.moveTo(-20, -20);
     ctx.lineTo(20, 20);
@@ -142,11 +175,14 @@ function drawX(x, y) {
     console.log("X-mark at cell (" + x + ", " + y + ")");
 }
 
-function drawO(x, y) {
-    var width = 600 / 8;
-    var height = 600 / 8;
+function drawO(x, y, color) {
+    var width = props.width / 8;
+    var height = props.height / 8;
     var ctx = myGameArea.context;
     var posX, posY;
+
+    if (!color) color = "#000";
+    ctx.strokeStyle = color;
 
     posX = x * width + width / 2;
     posY = y * height + height / 2;
