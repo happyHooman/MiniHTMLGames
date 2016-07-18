39cfeada0257d8b268d1a46d387606cc6643d2da
HappyHooman <tornea.alexandru@gmail.com>
2016-07-18 18:38:29 +0300

    addded adjustable size and number of rows and columns of the grid

--- FiveInARowGame/index.js
+++ FiveInARowGame/index.js
@@ -1,20 +1,20 @@
 var turn = "x";  //tracks who's turn is now
-var table = [
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""],
-    ["", "", "", "", "", "", "", ""]
-]; //initialize the grid table
-
 var marked = [];
 var props = {
     width: 600,
-    height: 600
+    height: 600,
+    rows: 10,
+    columns: 10
 };
+
+// initialize table
+var table = [];
+for (var i=0; i<props.rows;i++){
+    table.push([]);
+    for(var j=0; j<props.columns; j++){
+        table[i].push("");
+    }
+}
 
 
 function startGame() {
@@ -32,8 +32,8 @@ var myGameArea = {
         this.canvas.addEventListener('mousedown', function (e) {
             var x = e.offsetX;
             var y = e.offsetY;
-            var width = props.width / 8;
-            var height = props.height / 8;
+            var width = props.width / props.columns;
+            var height = props.height / props.rows;
             x = Math.floor(x / width);
             y = Math.floor(y / height);
             if (e.button == 0 && (turn == "x" || turn == "o")) {
@@ -50,19 +50,19 @@ var myGameArea = {
 function drawGrid() {
     var ctx = myGameArea.context;
     ctx.lineWidth = 3;
-    var width = props.width / 8;
-    var height = props.height / 8;
+    var width = props.width / props.columns;
+    var height = props.height / props.rows;
 
     //horizontal lines
-    for (var i = 0; i < 8; i++) {
+    for (var i = 0; i < props.rows; i++) {
         ctx.moveTo(0, i * height);
-        ctx.lineTo(8 * width, i * height);
+        ctx.lineTo(props.columns * width, i * height);
     }
 
     //vertical lines
-    for (i = 0; i < 8; i++) {
+    for (i = 0; i < props.columns; i++) {
         ctx.moveTo(i * width, 0);
-        ctx.lineTo(i * width, 8 * height);
+        ctx.lineTo(i * width, props.rows * height);
     }
 
     ctx.stroke();
@@ -73,11 +73,10 @@ function checkDirection(x, y, n1, n2) {
     // x, y - the coordinates of the current cell
     // n1, n2 - specify the direction (may only have values "-1", "0" and "1")
     var r = 0;
-    if (x + n1 >= 0 && x + n1 < 8 && y + n2 >= 0 && y + n2 < 8) {
+    if (x + n1 >= 0 && x + n1 < props.columns && y + n2 >= 0 && y + n2 < props.rows) {
         if (table[x + n1][y + n2] == table[x][y]) {
             marked.push([x + n1, y + n2]);
             r++;
-            // if (r == 4) return r;
             r += checkDirection(x + n1, y + n2, n1, n2)
         }
     }
@@ -85,16 +84,16 @@ function checkDirection(x, y, n1, n2) {
 }
 
 function printMarked(winner) {
-    var l = marked.length;
+    var length = marked.length;
     myGameArea.context.lineWidth = 10;
     myGameArea.context.lineCap = "round";
     if (winner == "x") {
-        for (var i = 0; i < l; i++) {
+        for (var i = 0; i < length; i++) {
             drawX(marked[i][0], marked[i][1], "red");
         }
     }
     if (winner == "o") {
-        for (i = 0; i < l; i++) {
+        for (i = 0; i < length; i++) {
             drawO(marked[i][0], marked[i][1], "red");
         }
     }
@@ -159,10 +158,11 @@ function draw(x, y) {
 }
 
 function drawX(x, y, color) {
-    var width = props.width / 8;
-    var height = props.height / 8;
+    var width = props.width / props.columns;
+    var height = props.height / props.rows;
     var ctx = myGameArea.context;
     var posX, posY;
+    var radius = 0.3 * width;
 
     if (!color) color = "#000";
     ctx.strokeStyle = color;
@@ -173,10 +173,10 @@ function drawX(x, y, color) {
 
     ctx.beginPath();
     ctx.translate(posX, posY);
-    ctx.moveTo(-20, -20);
-    ctx.lineTo(20, 20);
-    ctx.moveTo(20, -20);
-    ctx.lineTo(-20, 20);
+    ctx.moveTo(-radius, -radius);
+    ctx.lineTo(radius, radius);
+    ctx.moveTo(radius, -radius);
+    ctx.lineTo(-radius, radius);
     ctx.stroke();
     ctx.translate(-posX, -posY);
 
@@ -185,10 +185,11 @@ function drawX(x, y, color) {
 }
 
 function drawO(x, y, color) {
-    var width = props.width / 8;
-    var height = props.height / 8;
+    var width = props.width / props.columns;
+    var height = props.height / props.rows;
     var ctx = myGameArea.context;
     var posX, posY;
+    var radius = width * 0.35;
 
     if (!color) color = "#000";
     ctx.strokeStyle = color;
@@ -197,7 +198,7 @@ function drawO(x, y, color) {
     posY = y * height + height / 2;
 
     ctx.beginPath();
-    ctx.arc(posX, posY, 25, 0, 2 * Math.PI);
+    ctx.arc(posX, posY, radius, 0, 2 * Math.PI);
     ctx.stroke();
 
     table[x][y] = "o";
