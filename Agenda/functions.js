b7fdbe74f5bb55d0bb81454883c1c03007026c86
HappyHooman <tornea.alexandru@gmail.com>
2017-10-09 16:50:34 +0300

    contacts from Json

--- Agenda/functions.js
+++ Agenda/functions.js
@@ -2,16 +2,15 @@ function getRow(firstName, lastName, phone) {
     return '<tr><td>' + firstName + '</td><td>' + lastName + '</td><td>' + phone + '</td></tr>';
 }
 
-var contacte = [
-    ['Matei', 'Nicolae', '01'],
-    ['Tornea', 'Alexandru', '02'],
-    ['Bond', 'James', '03']
-];
 var tableContent = '';
 
-contacte.forEach(function (contact) {
-    tableContent += getRow(contact[0], contact[1], contact[2]);
+function createRow(contact) {
+    tableContent += getRow(contact.firstName, contact.lastName, contact.phone);
 }
-);
 
-$('#contacts-list tbody').html(tableContent);
+$.ajax('date/contacte.json').done(function (contacte) {
+        contacte.forEach(createRow);
+        $('#contacts-list tbody').html(tableContent);
+    }
+
+);
