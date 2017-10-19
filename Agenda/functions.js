bedc09141e9a5a8be670ef16776ceefce02269d7
HappyHooman <tornea.alexandru@gmail.com>
2017-10-19 15:01:37 +0300

    Add contacts and edit contacts

--- Agenda/functions.js
+++ Agenda/functions.js
@@ -3,7 +3,14 @@ function getRow(contact) {
     var phone= contact.phone || '';
     var firstName = contact.firstName || '';
     var lastName = contact.lastName || '';
-    return '<tr><td>' + firstName + '</td><td>' + lastName + '</td><td>' + phone + '</td>' + '<td>[<a href="date/remove.html?id=' + id + '">x</a>]</td></tr>';
+    return '<tr>' +
+        '<td>' + firstName + '</td>' +
+        '<td>' + lastName + '</td>' +
+        '<td>' + phone + '</td>' +
+        '<td class="actions">' +
+            '<span><a href="date/remove.html?id=' + id + '">&#x2716</a></span> ' +
+            '<span><a href="#">&#x270E</a></span>' +
+        '</td></tr>';
 }
 
 var tableContent = '';
