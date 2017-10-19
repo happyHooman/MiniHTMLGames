eb013582f917c73e36f093f47e162f9e3cc3ed2f
HappyHooman <tornea.alexandru@gmail.com>
2017-10-19 15:19:22 +0300

    code reformatting

--- Agenda/functions.js
+++ Agenda/functions.js
@@ -1,6 +1,6 @@
 function getRow(contact) {
     var id = contact.id;
-    var phone= contact.phone || '';
+    var phone = contact.phone || '';
     var firstName = contact.firstName || '';
     var lastName = contact.lastName || '';
     return '<tr>' +
@@ -8,8 +8,8 @@ function getRow(contact) {
         '<td>' + lastName + '</td>' +
         '<td>' + phone + '</td>' +
         '<td class="actions">' +
-            '<span><a href="date/remove.html?id=' + id + '">&#x2716</a></span> ' +
-            '<span><a href="#">&#x270E</a></span>' +
+        '<span><a href="date/remove.html?id=' + id + '">&#x2716</a></span> ' +
+        '<span><a class="edit" href="#">&#x270E</a></span>' +
         '</td></tr>';
 }
 
@@ -22,6 +22,18 @@ function createRow(contact) {
 $.ajax('date/contacte.json').done(function (contacte) {
         contacte.forEach(createRow);
         $('#contacts-list tbody').html(tableContent);
+        $('.edit').click(function () {
+            editContacts('Tornea', 'Alexandru', '05')
+        });
     }
-
 );
+
+function editContacts(firstName, lastName, phone) {
+    $('input[name=firstName]').val(firstName);
+    $('input[name=lastName]').val(lastName);
+    $('input[name=phone]').val(phone);
+}
+
+$('.edit').click(function () {
+    editContacts('Tornea', 'Alexandru', '05')
+});
