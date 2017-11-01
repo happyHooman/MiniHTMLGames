6781db9b68e2870a88ac26f122d3f6c25887f9bc
HappyHooman <tornea.alexandru@gmail.com>
2017-11-01 20:52:40 +0200

    add/edit

--- Agenda/date/addContact.php
+++ Agenda/date/addContact.php
@@ -11,23 +11,23 @@ $string = file_get_contents("contacte.json");
 $contacte = json_decode($string, true);
 
 
-if (isset($_GET["id"])){
-    //TODO update
-    $id= $_GET["id"];
+if (isset($_GET["id"])) {
 
-    for ($i=0; $i<count($contacte); $i++){
-        $contact= $contacte[$i];
-        if ($contact["id"]==$id){
+    $id = $_GET["id"];
+
+    for ($i = 0; $i < count($contacte); $i++) {
+        $contact = &$contacte[$i];
+        if ($contact["id"] == $id) {
             $contact["firstName"] = $_GET["firstName"];
             $contact["lastName"] = $_GET["lastName"];
             $contact["phone"] = $_GET["phone"];
         }
     }
-}else {
+} else {
     $idStr = file_get_contents("last.contact.id");
     $id = intval($idStr);
     $id++;
-    file_put_contents("last.contact.id",$id);
+    file_put_contents("last.contact.id", $id);
 
     $newPerson = array(
         "id" => $id,
@@ -35,11 +35,11 @@ if (isset($_GET["id"])){
         "lastName" => $_GET["lastName"],
         "phone" => $_GET["phone"]
     );
+
+    $contacte[] = $newPerson;
 }
 
 
-
-$contacte[] = $newPerson;
 $string = json_encode($contacte);
 file_put_contents("contacte.json", $string);
 header('Location: ../');
